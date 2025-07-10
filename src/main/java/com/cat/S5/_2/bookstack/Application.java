4c189cb661ce208aea7ed4d1e4f9e78fa3a2a2b3
zohra-b <z.bellamine@gmail.com>
2025-07-10 10:05:33 +0200

    pom configured

---
+++ src/main/java/com/cat/S5/_2/bookstack/Application.java
@@ -0,0 +1,13 @@
+package com.cat.S5._2.bookstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cat/S5/_2/bookstack/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.S5._2.bookstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
